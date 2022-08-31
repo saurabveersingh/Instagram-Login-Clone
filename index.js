898dcd8e7aaa7de22d27bda427e3f4edaaba91f4
saurabveersingh <saurabveersingh@gmail.com>
2022-08-31 12:51:36 +0530

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const user = document.querySelector(".user");
+    const pass = document.querySelector(".pass");
+    const sub = document.querySelector(".submit");
+    const form= document.querySelector(".form");
+
+    function redirectToInstagram(){
+        // add values to database
+
+        user.value=""
+        pass.value=""
+        enableSubmit();
+        // window.location = "https://www.instagram.com";
+    }
+
+    function enableSubmit(){
+        if(user.value!=="" && pass.value!=="" ){
+            sub.disabled=false;
+            sub.style.backgroundColor = '#0095f6'
+        }
+        else{
+            sub.style.backgroundColor = '#a0d5f8'
+            sub.disabled=true;
+        }
+    }
+    user.addEventListener('input',enableSubmit)
+    pass.addEventListener('input',enableSubmit)
+    sub.addEventListener('click',redirectToInstagram)
+})
